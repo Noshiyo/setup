51439bd13ae230d9b9f2ba61f6cbfcf8103c7afe
Noshiyo <julien.luccioni@protonmail.com>
2025-09-20 11:11:27 +0200

    Update 2_clone.ps1
    remove useless code

--- 2_clone.ps1
+++ 2_clone.ps1
@@ -8,8 +8,6 @@ $headers = @{Authorization = ("Basic {0}" -f $base64AuthInfo) }
 foreach ( $project in $projects ) {
     $result = Invoke-RestMethod -Uri "https://dev.azure.com/$organization/$project/_apis/git/repositories?api-version=6.0" -Method Get -Headers $headers
     
-    New-Item -ItemType Directory -Path $project
-
     foreach ($depot in $result.value.name) {
         git clone ("https://$organization@dev.azure.com/$organization/$project/_git/" + [uri]::EscapeDataString($depot)) "$project\$depot"
     }
